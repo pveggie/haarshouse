1af4bee1c1a73107c02bca00d5aed9b22f03b81f
Gemma Gotch <pveggie@hotmail.com>
2016-04-03 22:03:10 +0200

    haar jokes added, because, you know

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,30 @@
 class PagesController < ApplicationController
+  require 'json'
+  require 'open-uri'
+
   def home
+    joke = ""
+    chuck_norris = false
+
+    until chuck_norris
+      joke = get_joke
+      if joke =~ /chuck norris/i
+        chuck_norris = true
+      end
+      @haar_joke = joke.gsub(/chuck norris/i, "Haar")
+    end
+
+  end
+
+
+  private
+  def get_joke
+    api_url = 'http://api.icndb.com/jokes/random?escape=javascript'
+    joke = ""
+
+    open(api_url) do |stream|
+      joke = JSON.parse(stream.read)
+    end
+    joke['value']['joke']
   end
 end
