94a663652514ad01f0a23ca843939835652b48c6
Gemma Gotch <pveggie@hotmail.com>
2016-05-25 17:16:05 +0200

    moving joke to application controller

--- app/controllers/tunes_controller.rb
+++ app/controllers/tunes_controller.rb
@@ -4,7 +4,6 @@ class TunesController < ApplicationController
   before_action :find_tune, only: [:show, :edit, :update, :destroy]
 
   def index
-    @haar_joke = generate_joke
     @tunes = Tune.all
   end
 
@@ -38,45 +37,4 @@ class TunesController < ApplicationController
   def tune_params
     params.require(:tune).permit(:poster, :url, :poster_comment)
   end
-
-  def generate_joke
-    joke = ""
-    chuck_norris = false
-
-    until chuck_norris
-      joke = get_joke_from_api
-      return "Zzzzzzzzzzzzzzz." if joke.nil?
-
-      if joke =~ /chuck norris/i
-        #let's get rid of the racist, sexist and nasty ones...
-        chuck_norris = true unless joke =~ /race|woman|women|gay|black|natives|porn|handicap|god|bible|staring|rape|condom/i
-      end
-      haar_joke = joke.gsub(/chuck norrises/i, "Haars")
-      haar_joke = joke.gsub(/chuck norris\'s/i, "Haar's")
-      haar_joke = joke.gsub(/chuck norris/i, "Haar")
-      haar_joke = haar_joke.gsub(/penis|dick/i, "axe")
-      haar_joke = haar_joke.gsub(/american|america/i, "Daein")
-      haar_joke = haar_joke.gsub(/beat/i, "sleep")
-      haar_joke = haar_joke.gsub(/superman/i, "Chuck Norris")
-      haar_joke = haar_joke.gsub(/beard/i, "eyepatch")
-      haar_joke = haar_joke.gsub(/pick\-up/i, "wyvern")
-    end
-    return haar_joke
-  end
-
-
-  def get_joke_from_api
-    api_url = 'http://api.icndb.com/jokes/random?escape=javascript'
-    joke = ""
-
-    begin
-      open(api_url) do |stream|
-        joke = JSON.parse(stream.read)
-      end
-      joke['value']['joke']
-    rescue StandardError
-      return nil
-    end
-  end
-
 end
