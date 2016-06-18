1ac752ad9d2c94c58b78e5fb7cec9d038d4c6d27
Gemma Gotch <pveggie@hotmail.com>
2016-06-18 16:32:43 +0200

    Add methods and tests for extracting youtube video_id Check whether user enters valid youtube url, then extract video_id from this.

--- app/models/tune.rb
+++ app/models/tune.rb
@@ -8,19 +8,24 @@ class Tune < ActiveRecord::Base
 # == Relationships ========================================================
 
 # == Validations ==========================================================
-validates :game_title, presence: true
-validates :song_title, presence: true, uniqueness: { scope: :game_title,
-           message: "This song has already been added." }
-validates :youtube_video_id, presence: true,
-           uniqueness: { message: "This video has already been added." }
-
-# validates :poster, presence: true
+  validates :game_title, presence: true
+  validates :song_title, presence: true, uniqueness: { scope: :game_title,
+             message: "This song has already been added." }
+  validates :youtube_video_id, presence: true,
+             uniqueness: { message: "This video has already been added." },
+             format: {
+                      with: /.*youtu.*[=\/]([\w-]*)/, message: "Please check your youtube link."
+                    }
 
 # == Scopes ===============================================================
 
 # == Callbacks ============================================================
-
+  before_save :extract_video_id_from_youtube_url
 # == Class Methods ========================================================
 
 # == Instance Methods =====================================================
+  def extract_video_id_from_youtube_url
+    self.youtube_video_id = youtube_video_id.match(/.*youtu.*[=\/]([\w-]*)/)[1]
+  end
+
 end
--- spec/factories/tunes.rb
+++ spec/factories/tunes.rb
@@ -2,6 +2,6 @@ FactoryGirl.define do
   factory :tune do |f|
     f.game_title "Dragon Age 2"
     f.song_title "Fenris Theme"
-    f.youtube_video_id "dSCq7jTL7tQ"
+    f.youtube_video_id "youtube.com/watch?v=dSCq7jTL7tQ"
   end
 end
