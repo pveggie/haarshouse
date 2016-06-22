594e86e2350737b23a53517c4ebb85d9f647cf18
Gemma Gotch <pveggie@hotmail.com>
2016-06-22 21:50:15 +0200

    Add tests for edit view

--- spec/views/tunes/edit.html.erb_spec.rb
+++ spec/views/tunes/edit.html.erb_spec.rb
@@ -2,13 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "tunes/edit", type: :view do
   before(:each) do
-    @tune = assign(:tune, Tune.create!())
+    @tune = assign(:tune, FactoryGirl.build_stubbed(:tune))
   end
 
   it "renders the edit tune form" do
     render
-
-    assert_select "form[action=?][method=?]", tune_path(@tune), "post" do
-    end
+    assert_select "form[action=?][method=?]", tune_path(@tune), "post"
   end
 end
