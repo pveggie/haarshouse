b94139f2ee3b051efea794c4b1420facd17e64b1
Gemma Gotch <pveggie@hotmail.com>
2016-06-22 20:24:47 +0200

    Test for root route

--- spec/routing/tunes_routing_spec.rb
+++ spec/routing/tunes_routing_spec.rb
@@ -3,6 +3,10 @@ require "rails_helper"
 RSpec.describe TunesController, type: :routing do
   describe "routing" do
 
+    it "routes to #index as the homepage" do
+      expect(:get => "/").to route_to("tunes#index")
+    end
+
     it "routes to #index" do
       expect(:get => "/tunes").to route_to("tunes#index")
     end
