c7b259c4b8fae8b3abba580a8bb0bb4d2fdc71cc
Gemma Gotch <pveggie@hotmail.com>
2016-07-22 23:13:33 +0200

    specifying path to joke gem in gemfile for easier testing

--- Gemfile
+++ Gemfile
@@ -15,7 +15,7 @@ gem 'bootstrap-sass'
 gem 'font-awesome-sass'
 gem 'simple_form'
 gem 'autoprefixer-rails'
-gem 'haar_joke'
+gem 'haar_joke', path: '~/code/pveggie/haar_joke'
 
 group :development, :test do
   gem 'binding_of_caller'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::Base
-  require 'json'
-  require 'open-uri'
-  # require 'haar_joke'
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
