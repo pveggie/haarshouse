4909cdb1cb8538a02c255fbea12236827b10355a
Gemma Gotch <pveggie@hotmail.com>
2016-09-08 17:08:03 +0200

    Update view count on click. Add various tests for song display

--- spec/features/tune_playback_spec.rb
+++ spec/features/tune_playback_spec.rb
@@ -2,16 +2,32 @@ require 'rails_helper'
 
 RSpec.feature 'Tune playback', playing: true do
   background do
-    create(:tune)
+    create(:tune, views: 12)
   end
 
-  scenario "loading a youtube video by clicking the play button", js: true do
+  scenario "clicking the play button loads a youtube video", js: true do
     visit tunes_path
 
     within(:css, '.video-container') do
       expect(page).to_not have_css('iframe')
       find(:css, '.play-button').trigger('click')
       expect(page).to have_css('iframe')
+    end
+  end
+
+  scenario "the displayed view count is increased by one upon play", js: true do
+    visit tunes_path
+
+    within(:css, '.video-container') do
+      within(:css, '.caption-views') do
+        expect(page).to have_content(' 12')
+      end
+
+      find(:css, '.play-button').trigger('click')
+
+      within(:css, '.caption-views') do
+        expect(page).to have_content(' 13')
+      end
     end
   end
 end
--- spec/views/tunes/index.html.erb_spec.rb
+++ spec/views/tunes/index.html.erb_spec.rb
@@ -3,14 +3,22 @@ require 'rails_helper'
 RSpec.describe "tunes/index", type: :view, sorting: true, viewing: true do
 
   describe "List Rendering" do
-    it "renders a list of tunes" do
+    before do
       assign(:tunes, [
         build_stubbed(:tune, song_title: "Song A"),
         build_stubbed(:tune, song_title: "Song B")
       ])
+    end
+
+    it "renders a list of tunes" do
       render
       expect(rendered).to match(/Song A/)
       expect(rendered).to match(/Song B/)
+    end
+
+    it "renders play buttons" do
+      render
+      expect(rendered).to have_css('.play-button', count: 2)
     end
   end
 
@@ -31,6 +39,5 @@ RSpec.describe "tunes/index", type: :view, sorting: true, viewing: true do
       render
       expect(rendered).to match(/6544/)
     end
-
   end
 end
