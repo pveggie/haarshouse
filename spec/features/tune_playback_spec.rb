550ca94e3f3367894f380049bac1bbad9e9747fd
Gemma Gotch <pveggie@hotmail.com>
2016-09-08 19:43:37 +0200

    Add test for content within the youtube iframe

--- spec/features/tune_playback_spec.rb
+++ spec/features/tune_playback_spec.rb
@@ -5,14 +5,33 @@ RSpec.feature 'Tune playback', playing: true do
     create(:tune, views: 12)
   end
 
-  scenario "clicking the play button loads a youtube video", js: true do
+  scenario "clicking the play button loads youtube iframe", js: true do
     visit tunes_path
 
     within(:css, '.video-container') do
+      expect(page).to have_css('.video')
       expect(page).to_not have_css('iframe')
+
       find(:css, '.play-button').trigger('click')
+
       expect(page).to have_css('iframe')
     end
+  end
+
+  scenario "youtube video loads within iframe", js: true do
+    visit tunes_path
+
+    within(:css, '.video-container') do
+      find(:css, '.play-button').trigger('click')
+    end
+
+    within_frame 'dSCq7jTL7tQ' do
+      expect(page).to have_css('html')
+      # Following text is from YouTube video itself, so it will break
+      # if the song is renamed or deleted on youtube.
+      expect(page).to have_content('Dragon Age II Soundtrack')
+    end
+
   end
 
   scenario "the displayed view count is increased by one upon play", js: true do
