1883f9b98ecca9f1feea9ea4d9c350ee0ce4f50e
Gemma Gotch <pveggie@hotmail.com>
2016-09-08 14:54:25 +0200

    Show number of views

--- spec/views/tunes/index.html.erb_spec.rb
+++ spec/views/tunes/index.html.erb_spec.rb
@@ -1,13 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe "tunes/index", type: :view, sorting: true, viewing: true do
-  before do
-    assign(
-      :tunes, [build_stubbed(:tune), build_stubbed(:tune)]
-    )
+
+  describe "List Rendering" do
+    it "renders a list of tunes" do
+      assign(:tunes, [
+        build_stubbed(:tune, song_title: "Song A"),
+        build_stubbed(:tune, song_title: "Song B")
+      ])
+      render
+      expect(rendered).to match(/Song A/)
+      expect(rendered).to match(/Song B/)
+    end
   end
 
-  it "renders a list of tunes" do
-    render
+  describe "Tune details display" do
+    before { assign(:tunes, [build_stubbed(:tune, views: 6544)]) }
+
+    it "displays the game title" do
+      render
+      expect(rendered).to match(/Fenris Theme/)
+    end
+
+    it "displays the song title" do
+      render
+      expect(rendered).to match(/Dragon Age 2/)
+    end
+
+    it "displays the number of views" do
+      render
+      expect(rendered).to match(/6544/)
+    end
+
   end
 end
