f70a713fccc3940a40b9da1cc04cdacae4774809
Gemma Gotch <pveggie@hotmail.com>
2016-09-09 21:41:11 +0200

    Controller and View for Search -- search is now working

--- spec/features/tune_search_spec.rb
+++ spec/features/tune_search_spec.rb
@@ -2,8 +2,37 @@ require 'rails_helper'
 
 RSpec.feature 'Tune search', viewing: true, searching: true do
 
-  scenario "user searches a term and sees a list of matching videos" do
-    visit_tunes_path
-
-
+  background do
+    create(:tune)
+    create(:tune, song_title: "Isabela Theme", youtube_video_id: "eSCq7jTL7tQ")
+    create(:tune, song_title: "fenris Thing", youtube_video_id: "fSCq7jTL7tQ")
   end
+
+  scenario "user searches a term and sees a list of matching videos" do
+    visit tunes_path
+
+    within('.form-search') do
+      fill_in 'q', with: 'fenris'
+      click_on "btn-search-submit"
+    end
+
+    expect(page).to have_text("Fenris Theme")
+    expect(page).to have_text("fenris Thing")
+    expect(page).to_not have_text("Isabela Theme")
+  end
+
+  scenario "user sees all songs again after clicking filter" do
+    visit tunes_path
+
+    within('.form-search') do
+      fill_in 'q', with: 'fenris'
+      click_on "btn-search-submit"
+    end
+
+    expect(page).to_not have_text("Isabela Theme")
+
+    click_on "By Song"
+
+    expect(page).to have_text("Isabela Theme")
+  end
+end
