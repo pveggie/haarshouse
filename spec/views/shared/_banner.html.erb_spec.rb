5dce3d56c3729b20ad78a38a525c7659c4a6117b
Gemma Gotch <pveggie@hotmail.com>
2018-11-05 16:54:46 +0100

    Add login and logout and edit devise form layout

--- spec/views/shared/_banner.html.erb_spec.rb
+++ spec/views/shared/_banner.html.erb_spec.rb
@@ -1,18 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe 'shared/_banner', type: :view do
-  describe "Navigation" do
-
-    context "while logged out" do
-      it "renders the login icon" do
-        render partial: 'banner.html.erb', locals: { haar_joke: "test text" }
-        expect(rendered).to match /Login/
+  describe 'Navigation' do
+    context 'while logged out' do
+      before do
+        render partial: 'banner.html.erb', locals: { haar_joke: 'test text' }
       end
 
-      it "does not render a profile link" do
+      it 'renders the login icon' do
+        expect(rendered).to match(/Login/)
       end
 
-      it "does not render a logout link" do
+      it 'does not render a profile link' do
+        expect(rendered).to_not match(/Profile/)
+      end
+
+      it 'does not render a logout link' do
+        expect(rendered).to_not match(/Logout/)
+      end
+    end
+
+    context 'while logged in' do
+      before do
+        @user = assign(:user, create(:user))
+        sign_in @user
+        render partial: 'banner.html.erb', locals: { haar_joke: 'test text' }
+      end
+
+      it 'does not render the login icon' do
+        expect(rendered).to_not match(/Login/)
+      end
+
+      it 'renders a logout link' do
+        expect(rendered).to match(/Logout/)
       end
     end
   end
