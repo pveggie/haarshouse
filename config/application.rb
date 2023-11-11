139d13b300c5c5f2da9556b77975667560e00068
Gemma Gotch <pveggie@hotmail.com>
2023-11-11 16:55:12 +0100

    feat(rails 7): check old app config folder - remove old boilerplate files - remove old files with no differences

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module Haarshouse
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    
+    # for ajax
+    # config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
